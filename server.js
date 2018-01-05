40733c0bb508ab6827d980fbc96a3acafd905eec
claireellul <c.ellul@ucl.ac.uk>
2018-01-05 11:13:58 -0500

    flexible geoJSON generation from postGIS - just provide tablename and geometry column name - assumes public schema

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 
 var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
-//const config = "{"+configtext+"}";
 
 // now convert the configruation file into the correct format -i.e. a name/value pair array
 var configarray = configtext.split(",");
@@ -50,15 +49,20 @@ pool.connect(function(err,client,done) {
 });
 
 
-  app.get('/getPOI', function (req,res) {
+ app.get('/getPOI', function (req,res) {
      pool.connect(function(err,client,done) {
        if(err){
            console.log("not able to get connection "+ err);
            res.status(400).send(err);
        } 
         // use the inbuilt geoJSON functionality
-        //client.query('SELECT name, st_asgeojson(geom) as geom from united_kingdom_poi LIMIT 100', function(err,result){
-        	var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM (SELECT 'Feature' As type     , ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((SELECT l FROM (SELECT id, name, category) As l      )) As properties   FROM united_kingdom_poi  As lg limit 100  ) As f ";
+        // and create the required geoJSON format using a query adapted from here:  http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+        // note that query needs to be a single string with no line breaks so built it up bit by bit
+
+        	var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+        	querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.geom)::json As geometry, ";
+        	querystring = querystring + "row_to_json((SELECT l FROM (SELECT id, name, category) As l      )) As properties";
+        	querystring = querystring + "   FROM united_kingdom_poi  As lg limit 100  ) As f ";
         	console.log(querystring);
         	client.query(querystring,function(err,result){
 
@@ -68,12 +72,65 @@ pool.connect(function(err,client,done) {
                console.log(err);
                res.status(400).send(err);
            }
-           console.log(result);
            res.status(200).send(result.rows);
        });
     });
 });
 
+
+app.get('/getGeoJSON/:tablename/:geomcolumn', function (req,res) {
+     pool.connect(function(err,client,done) {
+      	if(err){
+          	console.log("not able to get connection "+ err);
+           	res.status(400).send(err);
+       	} 
+
+       	var colnames = "";
+
+       	// first get a list of the columns that are in the table 
+       	// use string_agg to generate a comma separated list that can then be pasted into the next query
+       	var querystring = "select string_agg(colname,',') from ( select column_name as colname ";
+       	querystring = querystring + " FROM information_schema.columns as colname ";
+       	querystring = querystring + " where table_name   = '"+ req.params.tablename +"'";
+       	querystring = querystring + " and column_name <>'"+req.params.geomcolumn+"') as cols ";
+
+        	console.log(querystring);
+        	
+        	// now run the query
+        	client.query(querystring,function(err,result){
+          //call `done()` to release the client back to the pool
+          	done(); 
+	          if(err){
+               	console.log(err);
+               		res.status(400).send(err);
+          	}
+           	colnames = result.rows;
+       	});
+        	console.log("colnames are " + colnames);
+
+        	// now use the inbuilt geoJSON functionality
+        	// and create the required geoJSON format using a query adapted from here:  
+        	// http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+        	// note that query needs to be a single string with no line breaks so built it up bit by bit
+
+        	var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+        	querystring = querystring + "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg." + req.params.geomcolumn+")::json As geometry, ";
+        	querystring = querystring + "row_to_json((SELECT l FROM (SELECT "+colnames + ") As l      )) As properties";
+        	querystring = querystring + "   FROM "+req.params.tablename+"  As lg limit 100  ) As f ";
+        	console.log(querystring);
+
+        	// run the second query
+        	client.query(querystring,function(err,result){
+	          //call `done()` to release the client back to the pool
+          	done(); 
+           	if(err){	
+                          	console.log(err);
+               		res.status(400).send(err);
+          	 }
+           	res.status(200).send(result.rows);
+       	});
+    });
+});
 
 
 app.get('/', function (req, res) {
