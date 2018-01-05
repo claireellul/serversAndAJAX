59e1db3ee0e7cc366982465e05b5a5b890929336
claireellul <c.ellul@ucl.ac.uk>
2018-01-05 14:51:27 -0500

    added new files to demonstrate uploading data to the server

---
+++ uploadData.js
@@ -0,0 +1,15 @@
+function startDataUpload() {
+	alert ("start data upload");
+
+	var name = document.getElementById("name").value;
+	var surname = document.getElementById("surname").value;
+	var module = document.getElementById("module").value;
+
+	alert(name + " "+ surname + " "+module);
+
+
+}
+
+
+
+
