45eb296de52ecb42bddcb75748922c3565331331
claireellul <c.ellul@ucl.ac.uk>
2018-01-05 16:23:09 -0500

    use javascript to post data to the server and save it to a postgres database

--- server.js
+++ server.js
@@ -38,30 +38,43 @@ httpsServer.listen(4443);
 
 
 app.post('/uploadData',function(req,res){
-
 	// note that we are using POST here as we are uploading data
 	// so the parameters form part of the BODY of the request rather than the RESTful API
-	console.log(req.body.name);
 	console.dir(req.body);
 
-	// for now, just echo the request back to the client
-	res.send(req.body);
+ 	pool.connect(function(err,client,done) {
+       	if(err){
+          	console.log("not able to get connection "+ err);
+           	res.status(400).send(err);
+       	} 
+       	var querystring = "INSERT into formdata (name,surname,module) values ('" + req.body.name + "','" + req.body.surname + "','" + req.body.module+"')";
+       	console.log(querystring);
+       	client.query( querystring,function(err,result) {
+          done(); 
+          if(err){
+               console.log(err);
+               res.status(400).send(err);
+          }
+          res.status(200).send("row inserted");
+       });
+    });
+
 });
 
 
 app.get('/postgistest', function (req,res) {
-pool.connect(function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       client.query('SELECT name FROM united_kingdom_counties' ,function(err,result) {
-           done(); 
-           if(err){
+ 	pool.connect(function(err,client,done) {
+       	if(err){
+          	console.log("not able to get connection "+ err);
+           	res.status(400).send(err);
+       	} 
+       	client.query('SELECT name FROM united_kingdom_counties' ,function(err,result) {
+          done(); 
+          if(err){
                console.log(err);
                res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
+          }
+          res.status(200).send(result.rows);
        });
     });
 });
