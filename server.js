4e58ac86e3b8708e86f46e2a2831bbf9d8abf952
claireellul <c.ellul@ucl.ac.uk>
2018-01-05 07:38:09 -0500

    server file adapted to test a simple connection to a database

--- server.js
+++ server.js
@@ -9,7 +9,46 @@ var privateKey = fs.readFileSync('/home/studentuser/certs/client-key.pem').toStr
 var certificate = fs.readFileSync('/home/studentuser/certs/client-cert.pem').toString(); 
 var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
+
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+//const config = "{"+configtext+"}";
+
+// now convert the configruation file into the correct format -i.e. a name/value pair array
+var configarray = configtext.split(",");
+var config = {};
+for (var i = 0; i < configarray.length; i++) {
+    var split = configarray[i].split(':');
+    config[split[0].trim()] = split[1].trim();
+}
+
+
+console.log(config);
+
+var pg = require('pg');
+var pool = new pg.Pool(config)
+
+
+
+
 httpsServer.listen(4443);
+
+app.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT name FROM united_kingdom_counties' ,function(err,result) {
+           done(); 
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
+
 
 app.get('/', function (req, res) {
   // run some server-side code
