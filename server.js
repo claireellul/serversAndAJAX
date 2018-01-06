d3ecc1f9e0e86306bbd5d53152c03396ffdebba4
claireellul <c.ellul@ucl.ac.uk>
2018-01-06 11:32:55 -0500

    server.js updated to remove the POST function detail for inserting data into the database

--- server.js
+++ server.js
@@ -36,39 +36,11 @@ app.use(bodyParser.json());
 httpsServer.listen(4443);
 
 
-/*app.post('/uploadData',function(req,res){
-	// note that we are using POST here as we are uploading data
-	// so the parameters form part of the BODY of the request rather than the RESTful API
-	console.dir(req.body);
-
- 	pool.connect(function(err,client,done) {
-       	if(err){
-          	console.log("not able to get connection "+ err);
-           	res.status(400).send(err);
-       	} 
-
-        // pull the geometry component together
-        // note that well known text requires the points as longitude/latitude !
-        // well known text should look like: 'POINT(-71.064544 42.28787)'
-        var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")'";
-
-       	var querystring = "INSERT into formdata (name,surname,module,language, modulelist, lecturetime, geom) values ('";
-       	querystring = querystring + req.body.name + "','" + req.body.surname + "','" + req.body.module + "','";
-       	querystring = querystring + req.body.language + "','" + req.body.modulelist + "','" + req.body.lecturetime+"',"+geometrystring + "))";
-       	console.log(querystring);
-       	client.query( querystring,function(err,result) {
-          done(); 
-          if(err){
-               console.log(err);
-               res.status(400).send(err);
-          }
-          res.status(200).send("row inserted");
-       });
-    });
-
+app.post('/uploadData',function(req,res){
+  databasecode.uploadData(req,res);
 });
 
-*/
+
 app.get('/postgistest', function (req,res) {
   databasecode.simpleQuery(req,res);
 });
